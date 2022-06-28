de6f03241140aac2128aaf658986d6c1e4620c33
Miriam Frömel <uzedf@student.kit.edu>
2022-06-28 15:38:40 +0200

    test of git

--- OOP_ClusteringV1.py
+++ OOP_ClusteringV1.py
@@ -1,3 +1,4 @@
+# 
 # OOP Clustering Code: H2 demand + el. demand
 # OOP Tutorial for Python: https://www.youtube.com/watch?v=Ej_02ICOIgs
 # Clustering Tutorial: https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
